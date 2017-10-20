fb257dbba4f779173aecb0d24693a5ad85d16cf7
Salva Corts <salvacorts97@gmail.com>
2017-10-20 22:50:14 +0200

    Made it on a more efficient way. Now complexy is O(n) instead of O(n^2)

--- fib.go
+++ fib.go
@@ -8,11 +8,19 @@ import (
 )
 
 func Fib(n int) int {
-	if n == 0 || n == 1 {
+  if n == 0 || n == 1 {
 		return n
 	}
 
-	return Fib(n-1) + Fib(n-2)
+  fibMem := make([]int, n+1);
+  fibMem[0] = 0;
+  fibMem[1] = 1;
+
+	for i := 2; i <= n; i++ {
+    fibMem[i] = fibMem[i-1] + fibMem[i-2];
+  }
+
+  return fibMem[n];
 }
 
 func main() {
